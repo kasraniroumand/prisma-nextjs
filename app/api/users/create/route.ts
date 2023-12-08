b8410ddc07e29aa0adbf292f79aec20a5195db2b
kassra niroumand <kassra.niroumand@gmail.com>
2023-12-08 22:53:41 +0000

    find unique

--- app/api/users/create/route.ts
+++ app/api/users/create/route.ts
@@ -3,21 +3,14 @@ import client from "@/prisma/client";
 
 export async function GET(request: NextRequest,
                           response: NextResponse){
-    const newUser = await client.user.createMany({
-        data: [
-            {
-                name: "test1",
-                email: "test1@gamil.com",
-                age : 20,
-            },{
-                name: "test2",
-                email: "test2@gamil.com",
-                age : 20,
-            }
-        ],
-    }).catch(e=>{
-        return NextResponse.json({error: e},{status: 400})
+    // find unique always return a single object
+    // just find based on unique attributes like id
+    const newUser = await client.user.findUnique({
+        where: {
+            id: 1
+        }
     })
+
     console.log(newUser)
     return NextResponse.json({newUser})
 }
