a9ddf388585a6c1aaed850a64a530474b47dbf1e
kassra niroumand <kassra.niroumand@gmail.com>
2023-12-08 22:48:24 +0000

    create many

--- app/api/users/create/route.ts
+++ app/api/users/create/route.ts
@@ -3,17 +3,18 @@ import client from "@/prisma/client";
 
 export async function GET(request: NextRequest,
                           response: NextResponse){
-    const newUser = await client.user.create({
-        data: {
-            name: "John2223445665 Doe",
-            email: "example225255446642@gamil.com",
-            age : 20,
-            userPreference:{
-                create:{
-                    emailupdates: true,
-                }
+    const newUser = await client.user.createMany({
+        data: [
+            {
+                name: "test1",
+                email: "test1@gamil.com",
+                age : 20,
+            },{
+                name: "test2",
+                email: "test2@gamil.com",
+                age : 20,
             }
-        },
+        ],
     }).catch(e=>{
         return NextResponse.json({error: e},{status: 400})
     })
